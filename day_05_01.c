2c2cd7abc8e49f92c4458bd3307740fcc4440c8b
markusz <markusz@playtiak.com>
2021-12-17 16:23:14 -0500

    Day 5 Part 2 (same file as Part 1, forgot to backup)

--- day_05_01.c
+++ day_05_01.c
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     char const* const fileName = argv[1];
     FILE* file = fopen(fileName, "r");
     char line[32];
-    coordinate map[99999] = {{-1, -1, 0}};
+    coordinate map[199999] = {{-1, -1, 0}};
     int mapWalker = 0;
 
     while (fgets(line, sizeof(line), file)) {
@@ -79,6 +79,57 @@ int main(int argc, char* argv[]) {
                     }
                 }
                 
+            }
+        }
+        /*
+            Comment next part if for part 1
+        */
+        else if (abs(x1 - x2) == abs(y1 - y2)) {
+            /* int minX = (x1 <= x2) ? x1 : x2;
+            int maxX = (x1 <= x2) ? x2 : x1;
+            int minY = (y1 <= y2) ? y1 : y2;
+            int maxY = (y1 <= y2) ? y2 : y1;
+            int x = minX;
+            int y = minY; */
+            int x = x1;
+            int y = y1;
+            while (1) {
+                int index = 0;
+                while (1) {
+                    if (map[index].x == x && map[index].y == y) {
+                        map[index].visited++;
+                        break;
+                    }
+                    index++;
+                    if (index > mapWalker) {
+                        map[mapWalker].x = x;
+                        map[mapWalker].y = y;
+                        map[index].visited = 1;
+                        mapWalker++;
+                        break;
+                    }
+                }
+                if (x == x2 && y == y2) {
+                    break;
+                }
+                if (x1 < x2) {
+                    x++;
+                    if (y1 < y2) {
+                        y++;
+                    }
+                    else {
+                        y--;    
+                    }
+                }
+                else {
+                    x--;
+                    if (y1 < y2) {
+                        y++;
+                    }
+                    else {
+                        y--;    
+                    }
+                }
             }
         }
     }
